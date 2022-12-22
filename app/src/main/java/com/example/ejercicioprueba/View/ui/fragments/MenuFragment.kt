a22ac6cd54f389c0566185171459cee48192130d
Gregory Reyes <greyes3@udi.edu.co>
2022-12-21 20:44:19 -0500

    Fragmentos de la aplicación

---
+++ app/src/main/java/com/example/ejercicioprueba/View/ui/fragments/MenuFragment.kt
@@ -0,0 +1,63 @@
+package com.example.ejercicioprueba.View.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.ejercicioprueba.R
+
+
+class MenuFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val cardAccesorios= view.findViewById<CardView>(R.id.fragAccesorios)
+        cardAccesorios.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_accesoriosFragment)
+        }
+        val cardBilleteras= view.findViewById<CardView>(R.id.fragBilleteras)
+        cardBilleteras.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_billeterasFragment)
+        }
+        val cardBolsos= view.findViewById<CardView>(R.id.fragBolsos)
+        cardBolsos.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_bolsosFragment)
+        }
+        val cardCalzado= view.findViewById<CardView>(R.id.fragCalzado)
+        cardCalzado.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_calzadoFragment)
+        }
+        val cardCamisas= view.findViewById<CardView>(R.id.fragCamisas)
+        cardCamisas.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_camisasFragment)
+        }
+        val cardCorreas= view.findViewById<CardView>(R.id.fragCorreas)
+        cardCorreas.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_correasFragment)
+        }
+        val cardGorras= view.findViewById<CardView>(R.id.fragGorras)
+        cardGorras.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_gorrasFragment)
+        }
+        val cardJeans= view.findViewById<CardView>(R.id.fragJeans)
+        cardJeans.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_jeansFragment)
+        }
+        val cardPerfumes= view.findViewById<CardView>(R.id.fragPerfumes)
+        cardPerfumes.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_perfumesFragment)
+        }
+    }
+
+}
